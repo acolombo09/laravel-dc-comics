0c3af4bea0c0fe1bd62d12d2f7fa054e32423d10
acolombo09 <acolombo0911@gmail.com>
2023-09-29 22:21:21 +0200

    seeders_done-milestone_1&2_completed

--- database/migrations/2023_09_29_172526_create_comic_table.php
+++ database/migrations/2023_09_29_172526_create_comic_table.php
@@ -13,19 +13,17 @@ return new class extends Migration
   {
     Schema::create('comics', function (Blueprint $table) {
       $table->id();
-
       // recupero tutto ciò che mi serve dal file comics per le tabelle
       // assegno le tipologie di dati
       $table->string('title', 60)->nullable(false);
-      $table->text('description')->nullable(false);
-      $table->string('thumb')->nullable(false);
+      $table->longtext('description')->nullable(false);
+      $table->text('thumb')->nullable(false);
       $table->decimal('price', 8,2)->nullable(false);
       $table->string('series', 60)->nullable(false);
       $table->date('sale_date')->nullable(false);
       $table->string('type', 30)->nullable(false);
       $table->json('artists')->nullable(); // tipo JSON per gli artisti multipli
       $table->json('writers')->nullable(); // tipo JSON per gli scrittori multipli
-
       $table->timestamps();
     });
   }
